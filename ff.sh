7c56025e0c59b742b801239b8ba8cf4bb43a849a
Vinit Joglekar <26863343+vinitjoglekar@users.noreply.github.com>
2023-04-09 16:46:21 +0530

    Created alpine-gui and firefox images

---
+++ ff.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Command line arguments
+# $1 - (Optional) Firefox context name. It can be an arbitrary alphanumeric string. If no 
+#      argument is provided, then creates a new random context name, which is actually a UUID.
+#      This script depends on "dbus-uuidgen" utility to generate a UUID on the fly.
+#      A context name is effectively an instance of firefox isolated from other instances.
+#      Each instance has an isolated cookie store, history, preferences etc.
+
+# Notes:
+#   1. Refer this to know how to use GUIs with Docker: http://wiki.ros.org/docker/Tutorials/GUI
+#   2. "-b" argument to sudo runs the command in the background freeing up the console terminal
+#   3. The DISPLAY and QT_X11_NO_MITSHM environment variables, along with the volume mount of
+#      /tmp/.X11-unix, ensures that the container OS connects to the hosts's Unix domain socket
+#      Refer: https://stackoverflow.com/a/25334301 and https://unix.stackexchange.com/a/196680
+#   4. firefox:latest image has been configured to run user process using user "u1"
+#
+
+if [ $# -eq 0 ]
+  then
+    uuid=$(dbus-uuidgen)
+    CONTEXT_NAME=$uuid
+else 
+    CONTEXT_NAME=$1
+fi
+
+mkdir --parents $HOME/docker_data/firefox/$CONTEXT_NAME/.mozilla
+
+sudo -b docker run --net=host \
+  --env="DISPLAY="$DISPLAY \
+  --env="QT_X11_NO_MITSHM=1" \
+  --rm \
+  --mount type=bind,src=$HOME/docker_data/firefox/$CONTEXT_NAME/.mozilla,dst=/home/u1/.mozilla \
+  --mount type=bind,src=$HOME/docker_data/Downloads,dst=/home/u1/shared/Downloads \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:ro" \
+  firefox:latest firefox
+
---
+++ ffm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Convenience script to launch Firefox container using "main" context.
+bash ./ff.sh main
