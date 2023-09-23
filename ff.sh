fc8f41d6d1c6c3876636b3fb185e940d21c29d1a
Vinit Joglekar <26863343+vinitjoglekar@users.noreply.github.com>
2023-09-23 21:43:00 +0530

    Updated firefox container definition, added python-audio container defn, modified run scripts

--- ff.sh
+++ ff.sh
@@ -4,7 +4,7 @@
 # $1 - (Optional) Firefox context name. It can be an arbitrary alphanumeric string. If no 
 #      argument is provided, then creates a new random context name, which is actually a UUID.
 #      This script depends on "dbus-uuidgen" utility to generate a UUID on the fly.
-#      A context name is effectively an instance of firefox isolated from other instances.
+#      A context name effectively creates an instance of firefox isolated from other instances.
 #      Each instance has an isolated cookie store, history, preferences etc.
 
 # Notes:
@@ -24,15 +24,16 @@ else
     CONTEXT_NAME=$1
 fi
 
-mkdir --parents $HOME/docker_data/firefox/$CONTEXT_NAME/.mozilla
+# Create a copy of the template .mozilla configuration directory for the container's use
+cp -R $HOME/docker_data/firefox/template $HOME/docker_data/firefox/$CONTEXT_NAME
 
 sudo -b docker run --net=host \
   --log-driver none \
   --env="DISPLAY="$DISPLAY \
   --env="QT_X11_NO_MITSHM=1" \
   --rm \
-  --mount type=bind,src=$HOME/docker_data/firefox/$CONTEXT_NAME/.mozilla,dst=/home/u1/.mozilla \
+  --mount type=bind,src=$HOME/docker_data/firefox/$CONTEXT_NAME,dst=/home/u1/.mozilla \
   --mount type=bind,src=$HOME/docker_data/Downloads,dst=/home/u1/shared/Downloads \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:ro" \
-  firefox-custom:latest firefox
+  firefox:latest firefox
 
--- runpy.sh
+++ runpy.sh
@@ -3,16 +3,30 @@
 # Refer below link to know how to use GUIs with Docker
 # http://wiki.ros.org/docker/Tutorials/GUI
 
+image=$1
+if [ "$image" = "base" ]; then
+  image="python-base"
+elif [ "$image" = "audio" ]; then
+  image="python-audio"
+fi
+
 sudo docker run -it \
   --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
   --rm \
   --mount type=bind,src=$HOME/github,dst=/github \
+  --mount type=bind,src=$HOME/docker_data/python,dst=/home/u1/python \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  $1 $2
+  $image $2
 
-# $1 --> image name - pytorch, scikit etc.
+# $1 --> image name
+#          audio (implies python-audio)
+#          base (implies python-base)
+#          pytorch 
+#          scikit
+#
 # $2 --> command to run - python, bash etc.
 # For example,
-# ./runpy.sh scikit bash
+# ./runpy.sh base bash
+
 
